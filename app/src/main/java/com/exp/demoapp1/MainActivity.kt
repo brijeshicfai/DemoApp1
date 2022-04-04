4a5683e5862b73f11a7b06b23e16860c869b0606
Admin <brijeshicfai>
2022-04-04 10:30:58 +0530

    mylearning

--- app/src/main/java/com/exp/demoapp1/MainActivity.kt
+++ app/src/main/java/com/exp/demoapp1/MainActivity.kt
@@ -2,12 +2,16 @@ package com.exp.demoapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //nest cahnge
+
         var temp: String="8"
+        Toast.makeText(applicationContext, "Email is valid", Toast.LENGTH_LONG).show()
+
+
     }
 }
