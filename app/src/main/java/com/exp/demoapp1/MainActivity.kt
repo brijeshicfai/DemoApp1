4fd3122f3cc47c99014beabd14627732ce05b8c2
Admin <brijeshicfai>
2022-04-04 09:34:30 +0530

    commit2

--- app/src/main/java/com/exp/demoapp1/MainActivity.kt
+++ app/src/main/java/com/exp/demoapp1/MainActivity.kt
@@ -2,10 +2,11 @@ package com.exp.demoapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-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
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //nest cahnge
+        var temp: String="8"
     }
 }
